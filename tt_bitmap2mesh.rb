e251ffdaa01cf056e70c71e07e69e56838360f99
Thomas Thomassen <192418+thomthom@users.noreply.github.com>
2013-04-18 17:40:29 +0200

    Updated PATH* constants

--- tt_bitmap2mesh.rb
+++ tt_bitmap2mesh.rb
@@ -19,11 +19,12 @@ module TT
   # Plugin information
   PLUGIN_ID       = File.basename( __FILE__ ).freeze
   PLUGIN_NAME     = 'Bitmap to Mesh'.freeze
-  PLUGIN_VERSION  = '0.5.0'.freeze
+  PLUGIN_VERSION  = '0.5.1'.freeze
   
   # Resource paths
-  PATH_ROOT   = File.dirname( __FILE__ ).freeze
-  PATH        = File.join( PATH_ROOT, PLUGIN_ID ).freeze
+  FILENAMESPACE = File.basename( __FILE__, '.rb' )
+  PATH_ROOT     = File.dirname( __FILE__ ).freeze
+  PATH          = File.join( PATH_ROOT, FILENAMESPACE ).freeze
   
   
   ### EXTENSION ### ------------------------------------------------------------

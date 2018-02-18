32c413f97ef2200dc1c152a5e8e7c55d50cba17d
Thomas Thomassen <thomas@thomthom.net>
2018-02-18 18:21:27 +0100

    Make PlaceMeshTool use the new HeightmapMesh class.

--- src/tt_bitmap2mesh/place_mesh_tool.rb
+++ src/tt_bitmap2mesh/place_mesh_tool.rb
@@ -5,8 +5,10 @@
 #
 #-------------------------------------------------------------------------------
 
+require 'tt_bitmap2mesh/helpers/image'
 require 'tt_bitmap2mesh/bitmap'
 require 'tt_bitmap2mesh/bitmap_render'
+require 'tt_bitmap2mesh/heightmap'
 
 
 module TT::Plugins::BitmapToMesh
@@ -281,172 +283,30 @@ module TT::Plugins::BitmapToMesh
       x_axis = box[0].vector_to(box[1])
       y_axis = box[0].vector_to(box[3])
       z_axis = box[0].vector_to(box[4])
-      width  = x_axis.length
-      height = y_axis.length
-      depth  = z_axis.length
+
       origin = box[0]
+      x_scale = x_axis.length / @bitmap.width
+      y_scale = y_axis.length / @bitmap.height
+      height  = z_axis.length
+      tr_scaling = Geom::Transformation.scaling(ORIGIN, x_scale, y_scale, 1)
+      tr_axes = Geom::Transformation.axes(origin, x_axis, y_axis, z_axis)
+      transformation = tr_axes * tr_scaling
+
       model = Sketchup.active_model
       model.start_operation('Mesh From Heightmap', true)
-      tr = Geom::Transformation.new(x_axis, y_axis, z_axis, origin)
-      group = bitmap_to_mesh(@bitmap, width, height, depth)
-      group.transformation = tr
+      heightmap = HeightmapMesh.new
+      material = get_or_create_material(model, @image, @bitmap)
+      group = heightmap.generate(model.active_entities, @bitmap, height,
+                                 material, transformation)
       model.commit_operation
       # Once the mesh is generated the tool is popped from the stack and
       # returned to the previous tool.
       model.tools.pop_tool
     end
 
-    def get_image_definition(image)
-      image.model.definitions.each { |definition|
-        if definition.image? && definition.instances.include?(image)
-          return definition
-        end
-      }
-      nil
-    end
-
-    def get_image_material(image)
-      definition = get_image_definition(image)
-      material_name = "b2m_#{definition.name}"
-      model = image.model
-      material = model.materials[material_name]
-      return material if material
-      material = model.materials.add(material_name)
-      if image.respond_to?(:image_rep)
-        material.texture = image.image_rep
-      else
-        Bitmap.temp_image_file(image) { |temp_file|
-          material.texture = temp_image_file
-        }
-      end
-      material
-    end
-
-    def create_material(bitmap)
-      material_name = "b2m_image"
-      model = Sketchup.active_model
-      material = model.materials.add(material_name)
-      bitmap.temp_file { |temp_file|
-        material.texture = temp_file
-      }
-      material
-    end
-
-    def get_material(image, bitmap)
-      # TODO: Clean up this kludgy mess!
-      image ? get_image_material(image) : create_material(bitmap)
-    end
-
-    def bitmap_to_mesh(bitmap, width, height, depth)
-      model = Sketchup.active_model
-
-      step_x = width  / bitmap.width
-      step_y = height / bitmap.height
-      step_z = depth  / 255
-
-      puts "Bitmap To Mesh: (#{bitmap.provider})"
-      puts "> Pixels: #{bitmap.width * bitmap.height} (#{bitmap.width}x#{bitmap.height})"
-      puts "> Triangles: #{bitmap.width * bitmap.height * 2}"
-      start_time = Time.now
-
-      # Read colour values and generate 3D points.
-      # TODO: Reuse logic in BitmapRender. It dupliates the greyscale logic.
-      #       Additionally the down-sampling logic can be used, but need UI
-      #       allow the user to control max-sample size.
-      points = []
-      w = bitmap.width
-      h = bitmap.height
-      u_step = 1.0 / bitmap.width.to_f
-      v_step = 1.0 / bitmap.height.to_f
-      # progress = TT::Progressbar.new(bitmap.pixels, 'Reading Image')
-      bitmap.height.times { |y|
-        bitmap.width.times { |x|
-          # progress.next
-          index = (bitmap.width * y) + x
-          color = bitmap.data[index]
-          # Generate a Point3d from pixel colour.
-          point_x = step_x * x
-          point_y = step_y * y
-          point_z = step_z * color.luminance
-          points << Geom::Point3d.new(point_x, point_y, point_z)
-        }
-      }
-      total = points.size.to_f
-      puts "> Processing data took: #{Time.now - start_time}s"
-
-      t = Time.now
-      # (!) Bottleneck!
-      # Populate the mesh with the point and build an vertex index.
-      # progress = TT::Progressbar.new(points, 'Indexing Points')
-      mesh = Geom::PolygonMesh.new(points.size, points.size * 2)
-      mesh_indicies = []
-      points.each_with_index { |point, i|
-        # progress.next
-        # Sketchup.status_text = sprintf("Indexing points: %.1f%%", (i / total) * 100.0)
-        mesh_indicies << mesh.add_point(point)
-
-        y = i / h
-        x = i - (h * y)
-        u = x * u_step
-        v = y * v_step
-        mesh.set_uv(i + 1, [u, v, 0], true)
-      }
-      puts "> Indexing points took: #{Time.now - start_time}s"
-
-      # Compute UV mapping:
-      #   t = Time.now
-      #   w = bitmap.width
-      #   h = bitmap.height
-      #   u_step = 1.0 / bitmap.width.to_f
-      #   v_step = 1.0 / bitmap.height.to_f
-      #   # progress = TT::Progressbar.new(points, 'Adding UV mapping')
-      #   points.each_with_index { |pt, i|
-      #     # progress.next
-      #     # Sketchup.status_text = sprintf("UV Mapping: %.1f%%", (i / total) * 100.0)
-      #     y = i / h
-      #     x = i - (h * y)
-      #     u = x * u_step
-      #     v = y * v_step
-      #     uv = Geom::Point3d.new(u, v, 0)
-      #     mesh.set_uv(i + 1, uv, true)
-      #   }
-      #   puts "> UV mapping took: #{Time.now - t}s"
-      # end
-
-      t = Time.now
-      # Generate the mesh
-      # progress = TT::Progressbar.new(bitmap.pixels, 'Generating Mesh')
-      0.upto(bitmap.height-2) { |y|
-        0.upto(bitmap.width-2) { |x|
-          # progress.next
-          r = y * bitmap.width # Current row
-          # Sketchup.status_text = sprintf("Generating mesh: %.1f%%", ((x+r) / total) * 100.0)
-          # Pick out the indicies from the patch 2D-matrix we're interested in.
-          point_indicies = [ x+r, x+1+r, x+bitmap.width+1+r, x+bitmap.width+r ]
-          # Get the point indicies and mirror orientation
-          indicies = point_indicies.map { |i| mesh_indicies[i] }
-          next unless indicies.length > 2
-          mesh.add_polygon(indicies[0], indicies[1], indicies[2])
-          mesh.add_polygon(indicies[0], indicies[2], indicies[3])
-        }
-      }
-      puts "> Generating mesh took: #{Time.now - t}s"
-
-      t = Time.now
-      Sketchup.status_text = 'Filling group with mesh...'
-      # Add the geometry to the model
-      group = model.active_entities.add_group
-      flags = 4 | 8 # AUTO_SOFTEN | SMOOTH_SOFT_EDGES
-      if mesh.respond_to?(:set_uv)
-        # material = image ? Image.clone_material(image) : Bitmap.create_material(model)
-        material = get_material(@image, @bitmap)
-        group.entities.fill_from_mesh(mesh, true, flags, material)
-      else
-        group.entities.fill_from_mesh(mesh, true, flags)
-      end
-      puts "> Filling mesh took: #{Time.now - t}s"
-      puts "Total time: #{Time.now - start_time}s"
-      group
+    def get_or_create_material(model, image, bitmap)
+      return nil unless Geom::PolygonMesh.instance_methods.include?(:set_uv)
+      material = image ? Image.clone_material(image) : bitmap.create_material(model)
     end
 
   end # class PlaceMeshTool
